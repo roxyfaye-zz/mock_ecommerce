bdc5f2b3fb43d215911810ead880bd7da2a726b2
roxyfaye <rdesantisnunally@gmail.com>
2017-09-06 11:40:02 -0400

    styling left

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("mySearch").placeholder = "Search files";
+}
+
+document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });
